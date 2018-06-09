95d3f66baa058c329b86f3777f2b6dc850cd4144
Chris Ng <chris.ng93@gmail.com>
2018-06-09 17:17:32 -0400

    update database init options

--- db/init.go
+++ db/init.go
@@ -12,9 +12,11 @@ var db *sql.DB
 
 // DatabaseFlagOptions defines the flag options for the database.
 type DatabaseFlagOptions struct {
+	DatabaseHost     string `long:"db_host" description:"The database host." default:"localhost" required:"false"`
+	DatabasePort     int64  `long:"db_port" description:"The database port." default:"5432" required:"false"`
 	DatabaseUser     string `long:"db_user" description:"The database user." required:"true"`
 	DatabasePassword string `long:"db_password" description:"The database user's password." required:"true"`
-	DatabaseName     string `long:"db_name" description:"The database name." required:"true"`
+	DatabaseName     string `long:"db_name" description:"The database name." default:"postgres" required:"false"`
 }
 
 // Init initializes the database instance.
@@ -22,7 +24,9 @@ func Init(options *DatabaseFlagOptions) (*sql.DB, error) {
 	var err error
 	connStr := fmt.Sprintf(
 		// TODO: Manage sslmode in prod.
-		"postgres://%s:%s@localhost/%s?sslmode=disable",
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		options.DatabaseHost,
+		options.DatabasePort,
 		options.DatabaseUser,
 		options.DatabasePassword,
 		options.DatabaseName,
