147b5428b4eef50896b1c1d897d9d74f7e3e237c
Chris Ng <chris.ng93@gmail.com>
2018-07-10 20:53:00 -0400

    uhoh

--- clients/yelp/get_business_details.go
+++ clients/yelp/get_business_details.go
@@ -6,16 +6,12 @@ import (
 	"github.com/chrisng93/coffee-backend/util"
 )
 
-func createGetBusinessDetailsURL(id string) string {
-	return fmt.Sprintf("/v3/businesses/%s", id)
-}
-
 // GetBusinessDetails calls Yelp's /v3/businesses/{id} endpoint to get more details on a
 // specific business.
 func (c *Client) GetBusinessDetails(id string) (*Business, error) {
 	req, err := util.CreateGetRequest(&util.GetRequestParams{
 		BaseURL: c.baseURL,
-		Path:    createGetBusinessDetailsURL(id),
+		Path:    fmt.Sprintf("/v3/businesses/%s", id),
 		APIKey:  c.apiKey,
 	})
 	if err != nil {
